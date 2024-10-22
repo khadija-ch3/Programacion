02cd23a8d0b6eccc9e64ba1341aff48c02f5a1b7
khadija <khache3@alu.edu.gva.es>
2024-10-22 12:59:05 +0200

    add a change

--- E_4_19/src/Main.java
+++ E_4_19/src/Main.java
@@ -3,6 +3,5 @@ import java.nio.file.attribute.UserDefinedFileAttributeView;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        UserDefinedFil
     }
 }

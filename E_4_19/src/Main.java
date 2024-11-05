68c65af969eded42dcd378f7e072c1b6fb59c550
khadija <khache3@alu.edu.gva.es>
2024-11-05 12:43:56 +0100

    adding a change

--- E_4_19/src/Main.java
+++ E_4_19/src/Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
 import java.nio.file.attribute.UserDefinedFileAttributeView;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        Scanner scanner = new Scanner(System.in);
+        int n= scanner.nextInt();
+        for (int i =1; i<=n;i++){
+            for (int j=1;j<i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+         }
 }

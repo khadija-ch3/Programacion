3177ea45a78e5b37057de92cd3cc8fe4260f13c5
khadija <khache3@alu.edu.gva.es>
2024-11-13 12:16:03 +0100

    resolving problems

--- ex14-arrayList/src/Main.java
+++ ex14-arrayList/src/Main.java
@@ -3,14 +3,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int contador = 0;
-        int[] numeros = new int[10];
-        for (int i = 0; i < numeros.length; i++) {
-            if (numeros[i] == numeros[numeros.length - 1]) {
-                contador++;
-            }
 
+        int n = scanner.nextInt();
+        int[] sec1 = new int[n];
+        for (int i = 0; i < sec1.length; i++) {
+            sec1[i] = scanner.nextInt();
         }
-        System.out.println(contador);
+
+        int m = scanner.nextInt();
+        int[] sec2 = new int[m];
+        for (int i = 0; i < sec2.length; i++) {
+            sec2[i] = scanner.nextInt();
+        }
+
+        // aqui va un if comprobando si m y n son distintos
+        // porque en ese caso no hace falta hacer el for
+
+        Boolean iguales = true;
+        for (int i = 0; i < n; i++) {
+            if (sec1[i] == sec2[i]) {
+                iguales = true;
+            } else {
+                iguales = false;
+            }
+        }
+        System.out.println(iguales);
+
+
     }
 }

35d682e4cd5e7e0891c919cbb86060cbbd08439d
khadija <khache3@alu.edu.gva.es>
2024-12-17 12:34:23 +0100

    add a change

--- ex13-metodos/src/Main.java
+++ ex13-metodos/src/Main.java
@@ -4,15 +4,15 @@ public class Main {
        String tex = "Soy productivo programando en Prolog";
 
        String pre= "pro";
-       String[] partes = tex.split(" "); // {"SOY", "PROUCTOVTIVO", "....
+       String[] partes = tex.split(" "); //String[] partes= {"SOY", "PROUCTOVTIVO", "en","prolog"...};
 
-        for (int i = 0; i < partes.length; i++) {
-            if (partes[i].toLowerCase().startsWith(pre)){
+        for (int i = 0; i< partes.length; i++) {
+            if (partes[i].toLowerCase().startsWith(pre.toLowerCase())) {
                 System.out.println(partes[i]);
             }
-
         }
-       //partir el texto en palabras con split
+
+        //partir el texto en palabras con split
         // hacer un for de las palabras
         //si la palabra pasa a minusculas es igual al prefijo
         //imprimir palabra
